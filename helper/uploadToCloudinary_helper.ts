fb05ac51efe3cde290fc4612f4c18f3b8ab6529a
2uy2 <quytran11204@gmail.com>
2026-01-14 02:08:22 +0700

    thêm upload audio

--- helper/uploadToCloudinary_helper.ts
+++ helper/uploadToCloudinary_helper.ts
@@ -10,15 +10,13 @@ cloudinary.config({
     api_secret: process.env.CLOUD_SECRET
 });
 
-let streamUpload = (buffer) => {
-    return new Promise((resolve, reject) => {
-        let stream = cloudinary.uploader.upload_stream(
+const streamUpload = (buffer: Buffer) => {
+    return new Promise<any>((resolve, reject) => {
+        const stream = cloudinary.uploader.upload_stream(
+            { resource_type: "auto" },
             (error, result) => {
-                if (result) {
-                    resolve(result);
-                } else {
-                    reject(error);
-                }
+                if (error) return reject(error);
+                resolve(result);
             }
         );
 
@@ -26,7 +24,7 @@ let streamUpload = (buffer) => {
     });
 };
 
-export const uploadToCloudinary = async (buffer: any) => {
-    let result = await streamUpload(buffer) || "";
-    return result["url"];
-}
+export const uploadToCloudinary = async (buffer: Buffer): Promise<string> => {
+    const result = await streamUpload(buffer);
+    return result.secure_url;
+};
--- middleware/admin/uploadCloud_middleware.ts
+++ middleware/admin/uploadCloud_middleware.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import { uploadToCloudinary } from "../../helper/uploadToCloudinary_helper";
 
-export const uploadSingle = async (req: Request,res: Response,next: NextFunction): Promise<void> => {
+export const uploadSingle = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     if (req["file"]) {
         const link = await uploadToCloudinary(req["file"].buffer);
         req.body[req["file"].fieldname] = link;
@@ -9,4 +9,24 @@ export const uploadSingle = async (req: Request,res: Response,next: NextFunction
     } else {
         next();
     }
+}
+export const uploadFields = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+): Promise<void> => {
+    
+    for (const key in req["files"]) {
+        const links = [];
+        for (const item of req["files"][key]) {
+            try {
+                const link = await uploadToCloudinary(item.buffer);
+                links.push(link);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        req.body[key] = links;
+    }
+    next();
 }
