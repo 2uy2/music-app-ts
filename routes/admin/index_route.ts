586bc3b2696c547ef64d5c0fa4edc8753951d10b
2uy2 <quytran11204@gmail.com>
2026-01-13 16:44:14 +0700

    tạo trang danh sách bài hát

--- routes/admin/index_route.ts
+++ routes/admin/index_route.ts
@@ -2,12 +2,14 @@ import { Express } from "express";
 import { dashboardRoutes } from "./dashboard_route";
 import{systemConfig} from "../../config/config";
 import { topicdRoutes } from "./topic_route";
+import { songRoutes } from "./song_routes";
 
 
 const adminRoutes = (app:Express)=>{
     const PATH_ADMIN= `/${systemConfig.prefixAdmin}`;
     app.use(`${PATH_ADMIN}/dashboard`,dashboardRoutes);
     app.use(`${PATH_ADMIN}/topics`,topicdRoutes);
+    app.use(`${PATH_ADMIN}/songs`,songRoutes);
 }
 
 export default adminRoutes;
